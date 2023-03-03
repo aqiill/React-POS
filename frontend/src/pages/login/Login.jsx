496c1ac5f80c333a29975f4a59e3fc101fe6c154
lutfi1661 <lutfi16.mlf@gmail.com>
2023-03-03 18:07:55 +0700

    update

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -2,73 +2,19 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function Login() {
-  // useEffect(() => {
-  //   document.title = "Login | BLEVEN";
-  //   document.body.classList.add("login-page", "hold-transition");
-  //   document.body.style.background = "#e7eef8";
-  //   return () => {
-  //     document.body.style.background = null;
-  //     document.body.classList.remove("login-page", "hold-transition");
-  //   };
-  // e3fd6b146fcb65f7419e3531a0a84f4d700b8210
-  // }, []);
-
-  // const [email, setEmail] = useState("");
-  // const [password, setPassword] = useState("");
-  // const [message, setMessage] = useState("");
-
-  // const handleSubmit = (event) => {
-  //   event.preventDefault();
-  //   const data = { email_user: email, password: password };
-  //   axios
-  //     .post("http://localhost:8080/login", data, {
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //         API_KEY: "e3fd6b146fcb65f7419e3531a0a84f4d700b8210",
-  //       },
-  //     })
-  //     .then((response) => {
-  //       if (response.data.status === 200) {
-  //         setMessage(response.data.message);
-  //         // do something with the user data returned by the API
-  //       } else {
-  //         setMessage(response.data.message);
-  //       }
-  //     })
-  //     .catch((error) => {
-  //       setMessage("Error: " + error.message);
-  //     });
-  // };
-
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    try {
-      const response = await axios.post(
-        "http://localhost:8080/login",
-        {
-          email_user: email,
-          password: password,
-        },
-        {
-          headers: {
-            API_KEY: "e3fd6b146fcb65f7419e3531a0a84f4d700b8210",
-          },
-        }
-      );
-
-      console.log(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  useEffect(() => {
+    document.title = "Login | BLEVEN";
+    document.body.classList.add("login-page", "hold-transition");
+    document.body.style.background = "#e7eef8";
+    return () => {
+      document.body.style.background = null;
+      document.body.classList.remove("login-page", "hold-transition");
+    };
+    // e3fd6b146fcb65f7419e3531a0a84f4d700b8210;
+  }, []);
 
   return (
     <>
-      {/*
       <div className="login-box">
         <div className="card">
           <div className="login-logo mt mb-5" style={{ marginTop: 50 }}>
@@ -85,7 +31,6 @@ function Login() {
               id="form-login"
               className="needs-validation"
               noValidate
-              onSubmit={handleSubmit}
             >
               <div className="form-group">
                 <label className="font-weight-lighter mb-0" htmlFor="email">
@@ -171,45 +116,6 @@ function Login() {
             </form>
           </div>
         </div>
-      </div>
-                */}
-      <div>
-        <h1>Login Page</h1>
-        {/* <form onSubmit={handleSubmit}>
-          <div>
-            <label>Email:</label>
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div>
-            <label>Password:</label>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <button type="submit">Login</button>
-        </form>
-        {message && <p>{message}</p>} */}
-        <form onSubmit={handleSubmit}>
-          <label>Email</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-          />
-          <label>Password</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-          <button type="submit">Login</button>
-        </form>
       </div>
     </>
   );
