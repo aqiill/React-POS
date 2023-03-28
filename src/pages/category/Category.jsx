cd367fe5f108a22ecc07193dd3c07ed5300b2699
Aryadipura <muhamadarya.8m19@gmail.com>
2023-03-28 20:17:54 +0700

    cat
    lom beres

--- src/pages/category/Category.jsx
+++ src/pages/category/Category.jsx
@@ -1,160 +1,273 @@
-import { useEffect, useState} from "react";
+import axios from "axios";
+import CommonComponent from "../../components/common/CommonComponent";
 import Header from "../../components/header/Header";
 import Sidebar from "../../components/sidebar/Sidebar";
+import React, { useEffect, useState } from "react";
+import Table from "./Table";
+import { Button, Modal} from "react-bootstrap";
 
 function Category() {
-const [] = useState(false);
+  const [category, setCategory] = useState([]);
+  const [formValues, setFormValues] = useState({
+    nama_kategori: ""
+  });
+  // const [successMessage, setSuccessMessage] = useState("");
+  // const [errorMessage, setErrorMessage] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const apiConfig = {
+    baseURL: "http://localhost:8080",
+    headers: {
+      api_key: "e3fd6b146fcb65f7419e3531a0a84f4d700b8210",
+    },
+  };
+  const [showModal, setShowModal] = useState(false);
 
-const handleSave = () => {
-//..
-}
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+    if (selectedCategory && selectedCategory[name] !== value) {
+      setSelectedCategory({
+        ...selectedCategory,
+        [name]: value,
+      });
+    }
+  };
 
-const destroy = () => {
-  //..
-  }
- 
+  const handleSubmit = async (event) => {
+    // event.preventDefault();
+    // try {
+    //   const response = await axios.post("/", formValues, apiConfig);
+    //   setSuccessMessage("Data kategori baru berhasil ditambahkan");
+    //   setFormValues({
+    //     nama_kategori: ""
+    //   });
+    //   setShowModal(true);
+    //   setErrorMessage("");
+    //   fetchData();
+    // } catch (error) {
+    //   console.error(error);
+    //   setSuccessMessage("");
+    //   setErrorMessage("Gagal menambahkan data kategori baru");
+    // }
+  };
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("/", apiConfig);
+      setCategory(response.data.data);
+    } catch (error) {
+      console.error(error);
+      setCategory([]);
+    }
+  };
+
   useEffect(() => {
-    document.title = "Category | POS";
-    document.body.classList.add(
-      "hold-transition",
-      "light-mode",
-      "sidebar-mini",
-      "layout-fixed",
-      "layout-navbar-fixed",
-      "layout-footer-fixed",
-      "sidebar-mini-xs"
-    );
-    document.body.style.background = "#e7eef8";
-    
-
-    return () => {
-      document.body.classList.remove(
-        "hold-transition",
-        "light-mode",
-        "sidebar-mini",
-        "layout-fixed",
-        "layout-navbar-fixed",
-        "layout-footer-fixed",
-        "sidebar-mini-xs"
-      );
-      document.body.style.background = null;
-    };
+    fetchData();
   }, []);
-  
+
   return (
     <>
+      <CommonComponent pageTitle="Category" backgroundStyle="#e7eef8" />
       <div className="wrapper">
         <Header />
         <Sidebar activePage="category" />
         <div className="content-wrapper row">
-        <section className="content col">
-        <div className="container-fluid">    
-        <div className="row content-card">
-          <div className="col-lg-12">
-            <div className="card"style={{boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: '10px',height: '755px'}}>
-              <div className="card-header border-0">
-                <div className="d-flex justify-content-between">
-                  <div className="add-export" style={{display: 'flex'}}>
-                    <button className="btn bg-transparent table-category-button" data-toggle="modal" data-target=".bd-example-modal-sm">
-                      <iconify-icon icon="oi:plus"></iconify-icon>
-                      Add Category
-                    </button>
-                    <div className="modal fade bd-example-modal-sm" id="myModal" tabIndex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
-                      <div className="modal-dialog modal-sm" role="document">
-                        <div className="modal-content">
-                          <div className="modal-header" style={{border: 'none'}}>
-                            <h5 className="modal-title">Add Category</h5>
-                          </div>
-                          <div className="modal-body">
-                            <div className="form-group">
-                              <label htmlFor="namecategory1">Category Name</label>
-                              <input type="name" className="form-control" id="namecategory1" aria-describedby="name" placeholder="Input Category Name"/>
+          <section className="content col">
+            <div className="container-fluid">
+              <div className="row content-card">
+                <div className="col-lg-12">
+                  <div
+                    className="card"
+                    style={{
+                      boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+                      borderRadius: 10,
+                      height: 755,
+                    }}
+                  >
+                    <div className="card-header border-0">
+                      <div className="d-flex justify-content-between">
+                        <div className="add-export" style={{ display: "flex" }}>
+                          <button
+                            className="btn bg-transparent table-cashier-button"
+                            data-toggle="modal"
+                            data-target=".bd-example-modal-sm"
+                          >
+                            <iconify-icon icon="oi:plus" />
+                            Add Category
+                          </button>
+                          <div
+                            className="modal fade bd-example-modal-sm"
+                            id="myModal"
+                            tabIndex={-1}
+                            role="dialog"
+                            aria-labelledby="mySmallModalLabel"
+                            aria-hidden="true"
+                          >
+                            <div
+                              className="modal-dialog modal-sm"
+                              role="document"
+                            >
+                              <div className="modal-content">
+                                <div
+                                  className="modal-header"
+                                  style={{ border: "none" }}
+                                >
+                                  <h5 className="modal-title">Add Category</h5>
+                                </div>
+                                <form onSubmit={handleSubmit}>
+                                  <div className="modal-body">
+                                    <div className="form-group">
+                                      <label htmlFor="name">Category Name</label>
+                                      <input
+                                        type="name"
+                                        className="form-control"
+                                        id="nama_user"
+                                        name="nama_user"
+                                        onChange={handleInputChange}
+                                        aria-describedby="name"
+                                        placeholder="Input Category Name"
+                                        autoComplete="off"
+                                      />
+                                    </div>
+                                  </div>
+                                  <div
+                                    className="modal-footer d-flex justify-content-between"
+                                    style={{ border: "none" }}
+                                  >
+                                    <button
+                                      type="button"
+                                      className="btn"
+                                      data-dismiss="modal"
+                                      style={{
+                                        backgroundColor: "white",
+                                        color: "black",
+                                        fontWeight: "normal",
+                                        fontSize: "smaller",
+                                        width: 100,
+                                        height: 35,
+                                        border: "none",
+                                      }}
+                                    >
+                                      Cancel
+                                    </button>
+                                    <button
+                                      type="submit"
+                                      className="btn ms-auto"
+                                      style={{
+                                        backgroundColor: "#5B7CFD",
+                                        color: "white",
+                                        fontWeight: "normal",
+                                        fontSize: "smaller",
+                                        width: 100,
+                                        height: 35,
+                                      }}
+                                      id="saveBtn"
+                                    >
+                                      Add
+                                    </button>
+                                  </div>
+                                </form>
+                                <Modal
+                                  show={showModal}
+                                  onHide={() => setShowModal(false)}
+                                >
+                                  <Modal.Header closeButton>
+                                    <Modal.Title>Sukses!</Modal.Title>
+                                  </Modal.Header>
+                                  <Modal.Body>
+                                    Data kategori baru berhasil ditambahkan.
+                                  </Modal.Body>
+                                  <Modal.Footer>
+                                    <Button
+                                      variant="primary"
+                                      onClick={() => setShowModal(false)}
+                                    >
+                                      OK
+                                    </Button>
+                                  </Modal.Footer>
+                                </Modal>
+                              </div>
                             </div>
-                          </div>
-                          <div className="modal-footer d-flex justify-content-between" style={{border: 'none'}}>
-                            <button type="button" className="btn" data-dismiss="modal" style={{backgroundColor: 'white', color: 'black', fontWeight: 'normal', fontSize: 'smaller', width: '100px', height: '35px', border: 'none'}}>Cancel</button>
-                            <button type="button" className="btn ms-auto" style={{backgroundColor: '#5B7CFD', color: 'white', fontWeight: 'normal', fontSize: 'smaller', width: '100px', height: '35px'}} id="saveBtn" onClick={() => handleSave()} data-dismiss="modal" >Add Items</button>
                           </div>
                         </div>
                       </div>
                     </div>
-                    <button className="btn bg-transparent table-category-button" onClick={() => window.print()}>
-                      <iconify-icon icon="oi:share-boxed"></iconify-icon>
-                      Export Category</button>
+                    <div className="card-body" style={{ padding: "0px 24px" }}>
+                      <Table category={category} />
+                    </div>
                   </div>
                 </div>
               </div>
-              <div className="card-body" style={{padding: '0px 24px'}}>
-                <table className="table" id="myTable" data-page-length="10" style={{width: '100%'}}>
-                  <thead>
-                    <tr className="text-muted fs-10">
-                      <td scope="col" style={{width: '10%'}}>No</td>
-                      <td scope="col" style={{width: '80%'}}>Category Name</td>
-                      <td scope="col" style= {{width: '10%'}}>Actions</td>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr className="fs-10">
-                      <td scope="row">1</td>
-                      <td>Food</td>
-                      <td>
-                        <button className="btn table-actions-button bg-transparent border drop-shadow"data-toggle="modal" data-target=".bd-example-modal-sm2" style={{borderRadius: '50%'}} >
-                          <iconify-icon icon="oi:pencil"></iconify-icon>
-                        </button>
-                        <div class="modal fade bd-example-modal-sm2" id="myModal" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
-                          <div class="modal-dialog modal-sm" role="document" >
-                            <div class="modal-content">
-                              <div class="modal-header" style={{border: 'none'}}>
-                                <h5 class="modal-title">Update Category</h5>
-                              </div>
-                              <div class="modal-body">
-                                <div class="form-group">
-                                  <label for="namecategory1">Category Name</label>
-                                  <input type="name" class="form-control" id="namecategory1" aria-describedby="name" placeholder="Input Category Name" value="Category"/>
-                                </div>
-                              </div>
-                              <div class="modal-footer d-flex justify-content-between"style={{border: 'none'}}>
-                                <button type="button" class="btn" data-dismiss="modal" style={{backgroundColor: 'white', color: 'black', fontWeight: 'normal', fontSize: 'smaller', width: '100px', height: '35px', border: 'none'}}>Cancel</button>
-                                <button type="button" class="btn ms-auto" style={{backgroundColor: '#5B7CFD', color: 'white', fontWeight: 'normal', fontSize: 'smaller', width: '100px', height: '35px'}} id="saveBtn" onClick={handleSave()} data-dismiss="modal">Update</button>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                        <button className="btn table-actions-button bg-transparent border drop-shadow ml-2 delete-row" data-toggle="modal" data-target=".delete" style={{borderRadius: '50%'}}>
-                          <iconify-icon icon="oi:trash"></iconify-icon>
-                        </button>
-                        <div class="modal delete"tabindex="-1" role="dialog" id="confirmModal">
-                          <div class="modal-dialog" role="document">
-                            <div class="modal-content">
-                              <div class="modal-header"style={{border: 'none'}}>
-                                <h5 class="modal-title">Delete Category</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                  <span aria-hidden="true">&times;</span>
-                                </button>
-                              </div>
-                              <div class="modal-body">
-                                <p>Are you sure want to delete this category ?</p>
-                              </div>
-                              <div class="modal-footer d-flex justify-content-between"style={{border: 'none'}}>
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal"style={{backgroundColor: 'white', color: 'black', fontWeight: 'normal', fontSize: 'smaller', width: '100px', height: '35px', border: 'none'}}>Cancel</button>
-                                <button type="button" class="btn btn-danger ms-auto" id="deleteBtn"  style={{backgroundColor: '#D94343', color: 'white', fontWeight: 'normal', fontSize: 'smaller', width: '100px', height: '35px'}} onClick={destroy()} data-dismiss="modal">Delete</button>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-              <div className="card-footer"  style={{backgroundColor: 'white', borderRadius:'10px', display: 'flex', justifyContent: 'center'}} >
-                <ul className="pagination" id="pagination"></ul>
+            </div>
+          </section>
+        </div>
+      </div>
+
+      <div
+        className="modal fade bd-example-modal-sm2"
+        id="myModal"
+        tabIndex={-1}
+        role="dialog"
+        aria-labelledby="mySmallModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-sm" role="document">
+          <div className="modal-content">
+            <div className="modal-header" style={{ border: "none" }}>
+              <h5 className="modal-title">Update Category</h5>
+            </div>
+            <div className="modal-body">
+              <div className="form-group">
+                <label htmlFor="nameCashier1">Category Name</label>
+                <input
+                  type="name"
+                  className="form-control"
+                  id="nameCashier1"
+                  aria-describedby="name"
+                  placeholder="Input Category Name"
+                />
               </div>
             </div>
+            <div
+              className="modal-footer d-flex justify-content-between"
+              style={{ border: "none" }}
+            >
+              <button
+                type="button"
+                className="btn"
+                data-dismiss="modal"
+                style={{
+                  backgroundColor: "white",
+                  color: "black",
+                  fontWeight: "normal",
+                  fontSize: "smaller",
+                  width: 100,
+                  height: 35,
+                  border: "none",
+                }}
+              >
+                Cancel
+              </button>
+              <button
+                type="button"
+                className="btn ms-auto"
+                style={{
+                  backgroundColor: "#5B7CFD",
+                  color: "white",
+                  fontWeight: "normal",
+                  fontSize: "smaller",
+                  width: 100,
+                  height: 35,
+                }}
+                id="saveBtn"
+              >
+                Update
+              </button>
+            </div>
           </div>
-          </div>
-        </div>
-        </section>
         </div>
       </div>
     </>
---
+++ src/pages/category/Table.jsx
@@ -0,0 +1,235 @@
+import "jquery/dist/jquery.min.js";
+import "datatables.net-dt/js/dataTables.dataTables";
+import "datatables.net-dt/css/jquery.dataTables.min.css";
+import "datatables.net-buttons/js/dataTables.buttons.js";
+import "datatables.net-buttons/js/buttons.colVis.js";
+import "datatables.net-buttons/js/buttons.flash.js";
+import "datatables.net-buttons/js/buttons.html5.js";
+import "datatables.net-buttons/js/buttons.print.js";
+import "pdfmake/build/pdfmake.min.js";
+import jsPDF from "jspdf";
+import "jspdf-autotable";
+import "jspdf-font";
+import React, { Component } from "react";
+
+import $ from "jquery";
+class Table extends Component {
+  componentDidMount() {
+    if (!$.fn.DataTable.isDataTable("#myTable")) {
+      $(document).ready(function () {
+        setTimeout(function () {
+          $("#table").DataTable({
+            destroy: true,
+            scrollY: "210px",
+            scrollCollapse: true,
+            paging: false,
+            processing: true,
+            dom:
+              "<'row'<'col-md-6'B><'col-md-6'f>>" +
+              "<'row'<'col-md-12't>>" +
+              "<'row'<'col-md-6'l><'col-md-6'p>>",
+
+            select: {
+              style: "single",
+            },
+
+            buttons: [
+              {
+                extend: "pdfHtml5",
+                text: "PDF",
+                orientation: "landscape",
+                pageSize: "A4",
+                titleAttr: "PDF",
+                className: "btn btn-secondary bg-secondary",
+                style: {
+                  backgroundColor: "white",
+                  color: "black",
+                  fontWeight: "normal",
+                  fontSize: "smaller",
+                  width: 100,
+                  height: 35,
+                  border: "none",
+                },
+                action: function (e, dt, button, config) {
+                  var data = dt.buttons.exportData();
+                  var headers = dt
+                    .columns()
+                    .header()
+                    .to$()
+                    .map(function () {
+                      return this.innerText;
+                    })
+                    .get();
+                  var excludedColumns = [1, 8];
+                  var columnIndexes = headers
+                    .map(function (column, index) {
+                      if (excludedColumns.includes(index)) {
+                        return null;
+                      }
+                      return index;
+                    })
+                    .filter(function (columnIndex) {
+                      return columnIndex !== null;
+                    });
+                  data = JSON.parse(
+                    JSON.stringify(data).replace(/<[^>]*>/g, "")
+                  );
+                  var doc = new jsPDF("l", "pt", this.pageSize);
+                  doc.autoTable(
+                    columnIndexes.map(function (columnIndex) {
+                      return headers[columnIndex];
+                    }),
+                    data.body.map(function (row) {
+                      return columnIndexes.map(function (columnIndex) {
+                        return row[columnIndex];
+                      });
+                    }),
+                    {
+                      startY: 60,
+                      margin: { top: 60 },
+                      styles: { overflow: "linebreak" },
+                      columnStyles: { 0: { cellWidth: 120 } },
+                      addPageContent: function (data) {
+                        doc.text("Category POS", 40, 30);
+                      },
+                    }
+                  );
+                  doc.save("Category POS.pdf");
+                },
+              },
+              {
+                extend: "csv",
+                text: "Excel",
+                exportOptions: {
+                  columns: [0, 2, 3, 4, 5, 6, 7],
+                  modifier: {
+                    selected: false,
+                  },
+                },
+                className: "btn btn-secondary bg-secondary",
+              },
+              {
+                extend: "print",
+                exportOptions: {
+                  columns: [0, 2, 3, 4, 5, 6, 7],
+                  modifier: {
+                    selected: false,
+                  },
+                },
+                customize: function (win) {
+                  $(win.document.body).css("font-size", "10pt");
+                  $(win.document.body)
+                    .find("table")
+                    .addClass("compact")
+                    .css("font-size", "inherit");
+                },
+                className: "btn btn-secondary bg-secondary",
+              },
+            ],
+            fnRowCallback: function (
+              nRow,
+              aData,
+              iDisplayIndex,
+              iDisplayIndexFull
+            ) {
+              var index = iDisplayIndexFull + 1;
+              $("td:first", nRow).html(index);
+              return nRow;
+            },
+            columnDefs: [
+              {
+                targets: 0,
+                render: function (data, type, row, meta) {
+                  return type === "export" ? meta.row + 1 : data;
+                },
+              },
+            ],
+            columns: [
+              { width: "10%" }, 
+              { width: "80%" }, 
+            ],
+          });
+        }, 1000);
+      });
+    }
+  }
+
+  showTable = () => {
+    try {
+      return this.props.category.map((item, index) => {
+        if (item.role === "Category") {
+          return (
+            <tr key={index}>
+              <td className="">{index + 1}</td>
+              <td className="">{item.nama_kategori}</td>
+              <td>
+                <button
+                  className="btn table-actions-button bg-transparent border drop-shadow"
+                  data-toggle="modal"
+                  data-target=".bd-example-modal-sm2"
+                  style={{ borderRadius: "50%", alignItems: "center" }}
+                >
+                  <iconify-icon icon="oi:pencil" />
+                </button>
+                <button
+                  className="btn table-actions-button bg-transparent border drop-shadow ml-2 delete-row"
+                  style={{ borderRadius: "50%" }}
+                  onClick={() => {
+                    if (
+                      window.confirm(
+                        "Are you sure want to delete this category?"
+                      )
+                    ) {
+                      this.handleDelete(index);
+                    }
+                  }}
+                >
+                  <iconify-icon icon="oi:trash" style={{ marginLeft: 2 }} />
+                </button>
+              </td>
+            </tr>
+          );
+        }
+      });
+    } catch (e) {
+      alert(e.message);
+    }
+  };
+
+  handleDelete = async (index) => {
+    // const { products } = this.props;
+    // const newProducts = [...products];
+    // newProducts.splice(index, 1);
+
+    // try {
+    //   await axios.delete(`http://localhost:8080/produk/${products[index].id}`);
+    //   this.setState({ products: newProducts });
+    // } catch (error) {
+    //   console.error("Error deleting product:", error);
+    // }
+  };
+
+  render() {
+    return (
+      <div className="card-body">
+        <div className=" scrollable-table" style={{ overflowX: "hidden" }}>
+          <table
+            id="table"
+            className="table align-items-center justify-content-center mb-0"
+          >
+            <thead>
+              <tr>
+                <th className="">No</th>
+                <th className="">Name</th>
+                <th className="">Action</th>
+              </tr>
+            </thead>
+            <tbody>{this.showTable()}</tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Table;
