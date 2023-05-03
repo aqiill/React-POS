fe1d1df7357489b6413c7aa91107fc9ba6fda11f
zidanalba_ <zmuhammadzidan@gmail.com>
2023-05-03 22:54:47 +0700

    Form Validation Added

--- src/pages/contact-us/Contact-Us.jsx
+++ src/pages/contact-us/Contact-Us.jsx
@@ -14,23 +14,16 @@ function ContactUs() {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs
-      .sendForm(
-        "service_d37pf4j",
-        "template_rbvia4p",
-        e.target,
-        "fGh5ZDdVenfF36cM7"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-          setIsEmailSentModalOpen(true);
-        },
-        (error) => {
-          console.log(error.text);
-          setIsEmailFailedModalOpen(true);
-        }
-      );
+    emailjs.sendForm("service_d37pf4j", "template_rbvia4p", e.target, "fGh5ZDdVenfF36cM7").then(
+      (result) => {
+        console.log(result.text);
+        setIsEmailSentModalOpen(true);
+      },
+      (error) => {
+        console.log(error.text);
+        setIsEmailFailedModalOpen(true);
+      }
+    );
     e.target.reset();
   };
 
@@ -44,10 +37,7 @@ function ContactUs() {
         <div className="content-wrapper row">
           <section className="content col">
             <section className="container-fluid">
-              <div
-                className="col-lg-12"
-                style={{ height: "calc(100vh - 102px)" }}
-              >
+              <div className="col-lg-12" style={{ height: "calc(100vh - 102px)" }}>
                 <div
                   className="card mb-0"
                   style={{
@@ -74,51 +64,28 @@ function ContactUs() {
                           <form ref={form} onSubmit={sendEmail}>
                             <div className="form-header">
                               <h5>Send a message</h5>
-                              <p className="text-muted">
-                                Fill up the form and our Team will get back to
-                                you within 24 hours
-                              </p>
+                              <p className="text-muted">Fill up the form and our Team will get back to you within 24 hours</p>
                             </div>
                             <div className="form-group help-form">
-                              <label
-                                htmlFor="name"
-                                style={{ fontWeight: "normal" }}
-                              >
+                              <label htmlFor="name" style={{ fontWeight: "normal" }}>
                                 Name
                               </label>
-                              <input
-                                className="form-control"
-                                name="user_name"
-                                placeholder="Input your name here"
-                              />
+                              <input className="form-control" name="user_name" placeholder="Input your name here" required />
+                              <div className="invalid-feedback">Please enter your name.</div>
                             </div>
                             <div className="form-group help-form">
-                              <label
-                                htmlFor="email"
-                                style={{ fontWeight: "normal" }}
-                              >
+                              <label htmlFor="email" style={{ fontWeight: "normal" }}>
                                 Gmail
                               </label>
-                              <input
-                                className="form-control"
-                                name="user_email"
-                                placeholder="Input your gmail here"
-                              />
+                              <input className="form-control" name="user_email" placeholder="Input your gmail here" required />
+                              <div className="invalid-feedback">Please enter your gmail.</div>
                             </div>
                             <div className="form-group help-form">
-                              <label
-                                htmlFor="message"
-                                style={{ fontWeight: "normal" }}
-                              >
+                              <label htmlFor="message" style={{ fontWeight: "normal" }}>
                                 Message
                               </label>
-                              <textarea
-                                className="form-control"
-                                name="message"
-                                rows={5}
-                                placeholder="Write your message"
-                                defaultValue={""}
-                              />
+                              <textarea className="form-control" name="message" rows={5} placeholder="Write your message" defaultValue={""} required />
+                              <div className="invalid-feedback">Tell us your problem here.</div>
                             </div>
                             <button
                               type="submit"
@@ -137,28 +104,15 @@ function ContactUs() {
                             </button>
                           </form>
                         </div>
-                        <div
-                          className="col-sm pt-8"
-                          style={{ paddingLeft: 32 }}
-                        >
+                        <div className="col-sm pt-8" style={{ paddingLeft: 32 }}>
                           <div className="social-media mb-4">
                             <div className="social-media-description mb-1">
                               <p className="mb-1">Whatsapp</p>
-                              <p className="mb-1 text-muted">
-                                Ask us via Whatsapp!
-                              </p>
+                              <p className="mb-1 text-muted">Ask us via Whatsapp!</p>
                             </div>
                             <div className="social-media-contact">
-                              <a
-                                href="https://wa.me/6289612744649"
-                                target="_blank"
-                              >
-                                <img
-                                  className="d-inline mr-2"
-                                  src="../dist/img/whatsapp.svg"
-                                  alt="whatsappLogo"
-                                  style={{ height: 20 }}
-                                />
+                              <a href="https://wa.me/6289612744649" target="_blank">
+                                <img className="d-inline mr-2" src="../dist/img/whatsapp.svg" alt="whatsappLogo" style={{ height: 20 }} />
                                 <p className="d-inline">089612744649</p>
                               </a>
                             </div>
@@ -166,22 +120,11 @@ function ContactUs() {
                           <div className="social-media mb-4">
                             <div className="social-media-description mb-1">
                               <p className="mb-1">Instagram</p>
-                              <p className="mb-1 text-muted">
-                                Follow us on Instagram!
-                              </p>
+                              <p className="mb-1 text-muted">Follow us on Instagram!</p>
                             </div>
                             <div className="social-media-contact">
-                              <a
-                                href="http://instagram.com/blevenpos"
-                                target="_blank"
-                                style={{ hover: "black" }}
-                              >
-                                <img
-                                  className="d-inline mr-2"
-                                  src="../dist/img/instagram.svg"
-                                  alt="instagramLogo"
-                                  style={{ height: 20 }}
-                                />
+                              <a href="http://instagram.com/blevenpos" target="_blank" style={{ hover: "black" }}>
+                                <img className="d-inline mr-2" src="../dist/img/instagram.svg" alt="instagramLogo" style={{ height: 20 }} />
                                 <p className="d-inline">@blevenpos</p>
                               </a>
                             </div>
@@ -202,11 +145,7 @@ function ContactUs() {
             <div className="modal-content">
               <div className="modal-header">
                 <h5 className="modal-title">Email Sent</h5>
-                <button
-                  type="button"
-                  className="close"
-                  onClick={() => setIsEmailSentModalOpen(false)}
-                >
+                <button type="button" className="close" onClick={() => setIsEmailSentModalOpen(false)}>
                   <span>&times;</span>
                 </button>
               </div>
@@ -228,26 +167,15 @@ function ContactUs() {
             <div className="modal-content">
               <div className="modal-header">
                 <h5 className="modal-title">Email Sending Failed</h5>
-                <button
-                  type="button"
-                  className="close"
-                  onClick={() => setIsEmailFailedModalOpen(false)}
-                >
+                <button type="button" className="close" onClick={() => setIsEmailFailedModalOpen(false)}>
                   <span>&times;</span>
                 </button>
               </div>
               <div className="modal-body">
-                <p>
-                  There was an error sending your message. Please try again
-                  later.
-                </p>
+                <p>There was an error sending your message. Please try again later.</p>
               </div>
               <div className="modal-footer">
-                <button
-                  type="button"
-                  className="btn btn-secondary"
-                  onClick={() => setIsEmailFailedModalOpen(false)}
-                >
+                <button type="button" className="btn btn-secondary" onClick={() => setIsEmailFailedModalOpen(false)}>
                   Close
                 </button>
               </div>
